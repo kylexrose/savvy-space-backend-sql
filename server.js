731e80be4916a9f5ffe800fe15dd1f4fd2f08b71
kylexrose@gmail.com <kylexrose@gmail.com>
2021-12-01 11:06:51 -0500

    added server management for sql db

---
+++ server.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456',
+    database : 'savvySpaceSql'
+});
+
+db.connect();
+
+module.exports = db;
